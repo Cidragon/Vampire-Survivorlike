291d4b702efe5270da66389f35b09e1d66ce8af7
Cidragon <darioperezpastrian@gmail.com>
2023-12-25 14:02:34 -0300

    start project, player movement

---
+++ scenes/player/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const speed : int = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	var accumulator : Vector2 = Vector2(0,0)
+	
+	if Input.is_action_pressed("up"):
+		accumulator.y -= speed
+	if Input.is_action_pressed("down"):
+		accumulator.y += speed
+	if Input.is_action_pressed("left"):
+		accumulator.x -= speed
+	if Input.is_action_pressed("right"):
+		accumulator.x += speed
+		
+	velocity = accumulator
