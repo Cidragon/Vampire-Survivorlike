f8e6ec31e2f9b93f6e67f1a9cf14cc3c77f1394d
Cidragon <darioperezpastrian@gmail.com>
2023-12-29 19:27:45 -0300

    enemy move to player by vector instead of matching coordinates

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -2,18 +2,11 @@ extends CharacterBody2D
 
 const speed : int = 100
 
+func _ready() -> void:
+	velocity = Vector2(speed,speed)
+
 func _process(delta: float) -> void:
 	var player_position : Vector2 = Global.player.global_position
-	
-	if player_position.y > global_position.y:
-		velocity.y = speed
-	elif player_position.y < global_position.y:
-		velocity.y = -speed
-	
-	if player_position.x > global_position.x:
-		velocity.x = speed
-	elif player_position.x < global_position.x:
-		velocity.x = -speed
-	
+	var target_position : Vector2 = (player_position - global_position).normalized()
+	velocity = target_position * speed
 	move_and_slide()
-	pass
